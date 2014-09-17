8de2580054ec498746a2ab033f9eace584f99c7e
Magnus Egelberg <magnus@egelberg.se>
2014-09-17 14:36:12 +0200

    -

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,5 @@
 class WelcomeController < ApplicationController
 
-	def hello
-		foo = {:kalle => 2}
-		
-		output foo
-		
-		rescue Exception => exception
-			error exception.message, :not_found
-		end	
-	end
+
 	
 end
