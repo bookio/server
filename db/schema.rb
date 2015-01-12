f107a66268ecc6c9b0e5264e0c4ad2a4fb55be8b
Magnus Egelberg <magnus@egelberg.se>
2015-01-12 14:01:15 +0100

    -

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216133302) do
+ActiveRecord::Schema.define(version: 20150112094952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 20141216133302) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "client_id"
+    t.integer  "units",       default: 0,  null: false
+    t.string   "unit",        default: "", null: false
+    t.integer  "selection",   default: 0,  null: false
   end
 
   create_table "rentals", force: true do |t|
