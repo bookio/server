873a0e1c9894dab60c9bc5100bc69b3340ffdd73
Magnus Egelberg <magnus@egelberg.se>
2015-01-20 13:16:03 +0100

    -

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -6,7 +6,18 @@ class Rental < ActiveRecord::Base
 	has_many :reservations, :dependent => :destroy
 	#has_many :options, :dependent => :destroy
 	
-	has_many :schedules, :dependent => :destroy
+	#has_many :schedules, :dependent => :destroy
+	
+	before_destroy :cleanup
+	
+	
+	#def schedule
+	#	session.user.client.schedules.new(schedule_params)
+	#end
+	
+	def cleanup
+		Schedule.delete_all(["entity = 'rental' AND entity_id = ?", self.id])
+	end
 	
 	
 	belongs_to :client
